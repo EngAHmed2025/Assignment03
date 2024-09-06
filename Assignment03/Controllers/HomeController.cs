7a906eb6ed4f13f3db05f4e42c6d4b461f1b8b23
EngAHmed2025 <elshafeyahmed110@gmail.com>
2024-09-06 14:14:06 +0300

    Add project files.

---
+++ Assignment03/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
